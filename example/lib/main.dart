a4629fb3506729d439c392c2bc9284ea2d213b5b
nam-tu <nam.tu@ashored.ca>
2022-08-15 15:37:44 -0300

    update flutter_map to ^0.14.0

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:flutter_map_mbtiles_provider/flutter_map_mbtiles_provider.dart';
 
 void main() {
